c00c8d7cccd15dd42cadb5e5876b76015bfee47a
Ismail Mustafa <ismailmustafa@rocketmail.com>
2022-07-27 21:16:57 -0400

    Add more footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,6 +11,18 @@ export default function Footer() {
         <li className={styles.navItem}>
           <a href="https://github.com/gobeyondidentity">GitHub</a>
         </li>
+        <li className={styles.navItem}>
+          <a href="https://stackoverflow.com/questions/tagged/beyondidentity">Stack Overflow</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="https://join.slack.com/t/byndid/shared_invite/zt-1anns8n83-NQX4JvW7coi9dksADxgeBQ">Slack</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="https://twitter.com/BI_Developers">Twitter</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="https://www.beyondidentity.com/developers/blog">Blog</a>
+        </li>
       </ul>
     </footer>
   )
