cc2636c8d5401cb45e35bb36f4184a2a1aa61130
Nelson Melo <nmelo.cu@gmail.com>
2022-08-02 19:28:09 -0400

    Simplified and centered footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,27 +3,24 @@ import styles from "./footer.module.css"
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <hr />
-      <ul className={styles.navItems}>
-        <li className={styles.navItem}>
-          <a href="https://developer.beyondidentity.com">Documentation</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://github.com/gobeyondidentity">GitHub</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://stackoverflow.com/questions/tagged/beyondidentity">Stack Overflow</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://join.slack.com/t/byndid/shared_invite/zt-1anns8n83-NQX4JvW7coi9dksADxgeBQ">Slack</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://twitter.com/BI_Developers">Twitter</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://www.beyondidentity.com/developers/blog">Blog</a>
-        </li>
-      </ul>
+      <div className={styles.navItem}>
+        <a href="https://developer.beyondidentity.com">Documentation</a>
+      </div>
+      <div className={styles.navItem}>
+        <a href="https://github.com/gobeyondidentity">GitHub</a>
+      </div>
+      <div className={styles.navItem}>
+        <a href="https://stackoverflow.com/questions/tagged/beyondidentity">Stack Overflow</a>
+      </div>
+      <div className={styles.navItem}>
+        <a href="https://join.slack.com/t/byndid/shared_invite/zt-1anns8n83-NQX4JvW7coi9dksADxgeBQ">Slack</a>
+      </div>
+      <div className={styles.navItem}>
+        <a href="https://twitter.com/BI_Developers">Twitter</a>
+      </div>
+      <div className={styles.navItem}>
+        <a href="https://www.beyondidentity.com/developers/blog">Blog</a>
+      </div>
     </footer>
   )
 }
